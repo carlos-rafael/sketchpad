597b1ba549e13dc0c3d12f6d8612da5cfc589ccc
Carlos Antunes <carlos.rafael@gec.inatel.br>
2017-05-25 23:05:21 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+
+    for(var i=0;i<10;i++){
+
+        $('#gridContainer').append($("<div class='square'>"));
+    }
+            $('#gridContainer').append($("<div class='square1'>"));
+});
